1bc068d9d7536d0893b37327a2b9c6c84e9873a8
hyunmin2439 <72420501+hyunmin2439@users.noreply.github.com>
2021-06-07 22:40:52 +0900

    Add files via upload

---
+++ Baekjoon15596.java
@@ -0,0 +1,17 @@
+package com.algo;
+
+public class Baekjoon15596 {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	long sum(int[] a) {
+		int ans = 0;
+		
+		for(int num : a)
+			ans += num;
+		
+		return ans;
+	}
+}
