116948c44e3d212ffc2c880bc2719b2a03de3278
hyunmin2439 <72420501+hyunmin2439@users.noreply.github.com>
2021-06-22 15:03:44 +0900

    Add files via upload

---
+++ Baekjoon10757.java
@@ -0,0 +1,24 @@
+package solved;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class Baekjoon10757 {
+
+	public static void main(String[] args) throws IOException {
+		BigInteger b1, b2, b3;
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String[] line = br.readLine().trim().split(" ");
+		
+		b1 = new BigInteger( line[0].toString() );
+		b2 = new BigInteger( line[1].toString() );
+		b3 = b1.add(b2);
+		
+		bw.write(b3.toString());
+		br.close();
+		bw.close();
+	}
+}
