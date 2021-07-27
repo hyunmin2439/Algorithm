8ecb482268ef94ab3e927931a3bc3a02ee1428b7
hyunmin2439 <72420501+hyunmin2439@users.noreply.github.com>
2021-07-27 23:44:43 +0900

    Add files via upload

---
+++ Baekjoon11719.java
@@ -0,0 +1,27 @@
+package solved.submit;
+
+import java.io.*;
+
+public class Baekjoon11719 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+				
+		for (int i = n; i > 0; i--) {
+			for (int j = 0; j < n - i; j++) {
+				sb.append(" ");
+			}
+			for (int j = 0; j < i * 2 - 1; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		
+		bw.write(sb.toString());
+		br.close();
+		bw.close();
+	}
+}
