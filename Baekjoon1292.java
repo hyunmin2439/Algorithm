5f086d03c0e23a2f410e17d7435fd8143db969d6
hyunmin2439 <72420501+hyunmin2439@users.noreply.github.com>
2021-07-29 23:23:46 +0900

    Add files via upload

---
+++ Baekjoon1292.java
@@ -0,0 +1,39 @@
+package com.algo;
+
+import java.io.*;
+
+public class Baekjoon1292 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader( new InputStreamReader(System.in) );
+		BufferedWriter bw = new BufferedWriter( new OutputStreamWriter(System.out) );
+		
+		int[] arr = new int[1001];
+		
+		String[] line = br.readLine().split(" ");
+		int A = Integer.parseInt(line[0]);
+		int B = Integer.parseInt(line[1]);
+	
+		int idx = 1;
+		int num = 1;
+		while(idx <= 1000) {
+			
+			for (int i = 1; i <= num && idx <= 1000; i++) {
+				arr[idx] = num;
+				idx++;
+			}
+			
+			num++;
+		}
+		
+		int sum = 0;
+		for (int i = A; i <= B; i++) {
+			sum += arr[i];
+		}
+
+		bw.write(String.valueOf(sum));
+		br.close();
+		bw.close();
+	}
+
+}
