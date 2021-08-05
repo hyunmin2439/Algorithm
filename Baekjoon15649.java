7c89a5a0c03cfd93ec30217a0fae00fd19fdb536
hyunmin2439 <72420501+hyunmin2439@users.noreply.github.com>
2021-08-05 23:11:02 +0900

    Add files via upload

---
+++ Baekjoon15649.java
@@ -0,0 +1,57 @@
+package solved.notsubmit;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Baekjoon15649 {
+
+	static int N, M;
+	static int[] numArr;
+	static boolean[] isSelected;
+	
+	static BufferedReader br;
+	static BufferedWriter bw;
+	static StringBuilder  sb;
+	
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader( new InputStreamReader( System.in) );
+		bw = new BufferedWriter( new OutputStreamWriter( System.out) );
+		sb = new StringBuilder();
+		
+		String[] line = br.readLine().split(" ");
+		N = Integer.parseInt(line[0]);
+		M = Integer.parseInt(line[1]);
+		
+		numArr = new int[M];
+		isSelected = new boolean[N + 1]; // 0 : dummy
+		
+		permutation(0);
+		
+		bw.write(sb.toString());
+		br.close();
+		bw.close();
+	}
+
+	private static void permutation(int cnt) {
+		if(cnt == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(numArr[i]).append(" ");				
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			if(isSelected[i]) continue;
+			
+			numArr[cnt] = i;
+			isSelected[i] = true;
+			permutation(cnt + 1);
+			isSelected[i] = false;
+		}
+	}
+
+}
