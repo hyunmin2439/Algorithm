692d3d4f21972bf80d5b37f853b69ab195d330fd
hyunmin2439 <72420501+hyunmin2439@users.noreply.github.com>
2021-08-20 22:55:05 +0900

    Add files via upload

---
+++ Backjoon10988.java
@@ -0,0 +1,29 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Backjoon10988 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String line = br.readLine();
+		int res = 1, i = 0, n = line.length() - 1;
+		
+		while(i < n) {
+			
+			if(line.charAt(i) != line.charAt(n)) {
+				res = 0;
+				break;
+			}
+			
+			i++; n--;
+		}
+		
+		System.out.println(res);
+		br.close();
+	}
+
+}
