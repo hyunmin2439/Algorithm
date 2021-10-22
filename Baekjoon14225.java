73da7d588c9e67df6ce499f4162b3916a23aec39
hyunmin2439 <72420501+hyunmin2439@users.noreply.github.com>
2021-10-22 23:31:38 +0900

    Add files via upload

---
+++ Baekjoon14225.java
@@ -0,0 +1,50 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// Memory:18832KB / Time:264ms
+public class Baekjoon14225 {
+
+	static final int VISIT_MAX = 100_000;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		// input
+		int N = Integer.parseInt(in.readLine());
+
+		int[] arr = new int[N];
+		boolean[] visit = new boolean[VISIT_MAX * N + 1];
+
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		// process
+		int flag = 0;
+		int end = 1 << N;
+		
+		while(++flag < end) {
+			int sum = 0;
+			
+			for (int i = 0; i < N; i++) {
+				if( (flag & 1 << i) != 0 )
+					sum += arr[i];
+			}
+			
+			visit[sum] = true;
+		}
+		
+		// output
+		int ans = 1;
+		while( visit[ans] ) ans++;
+		
+		System.out.println(ans);
+		
+		in.close();
+	}
+
+}
