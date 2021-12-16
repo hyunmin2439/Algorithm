91d38cc5013f7fdecad2fadcc9dc6afd5e57d4e2
hyunmin2439 <72420501+hyunmin2439@users.noreply.github.com>
2021-12-16 21:18:19 +0900

    Update and rename Baekjoon11050_second.java to BJ_11050_이항계수1_2_DP.java

--- BJ_11050_이항계수1_2_DP.java
+++ BJ_11050_이항계수1_2_DP.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Baekjoon11050_second {
+public class BJ_11050_이항계수1_2_DP {
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
