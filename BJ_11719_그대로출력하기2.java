8f09451607675e425289cb6ff3d326679075fd3d
hyunmin2439 <72420501+hyunmin2439@users.noreply.github.com>
2021-12-20 21:31:13 +0900

    Update and rename Baekjoon11719.java to BJ_11719_그대로출력하기2.java

--- BJ_11719_그대로출력하기2.java
+++ BJ_11719_그대로출력하기2.java
@@ -2,7 +2,7 @@ package solved.submit;
 
 import java.io.*;
 
-public class Baekjoon11719 {
+public class BJ_11719_그대로출력하기2 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
