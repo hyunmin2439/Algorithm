aaf6fce1ace99a5551471fc8a2a52fd7e3fb4566
hyunmin2439 <72420501+hyunmin2439@users.noreply.github.com>
2021-12-24 23:39:19 +0900

    Update and rename Baekjoon15686.java to BJ_15686_치킨배달.java

--- BJ_15686_치킨배달.java
+++ BJ_15686_치킨배달.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 // first Solve
-public class Baekjoon15686 {
+public class BJ_15686_치킨배달 {
 	
 	static int N, M, ckCnt, ckDist;
 	static char[][] city;
