2f6c6c467d2adecaf3dd8833e98cdd7a0860047a
hyunmin2439 <72420501+hyunmin2439@users.noreply.github.com>
2021-12-28 18:10:47 +0900

    Update BJ_16234_인구이동_1_BFS.java

--- BJ_16234_인구이동_1_BFS.java
+++ BJ_16234_인구이동_1_BFS.java
@@ -1,5 +1,3 @@
-package algo;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -7,6 +5,10 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
+
+/*
+ * Memory:202512KB / Time:492ms
+ */
 
 public class BJ_16234_인구이동_1_BFS {
 
