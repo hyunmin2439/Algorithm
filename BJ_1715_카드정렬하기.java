1a9650c88d75eb7e30ad8852df2fb990ad839e26
hyunmin2439 <72420501+hyunmin2439@users.noreply.github.com>
2022-01-13 23:39:03 +0900

    Update and rename Baekjoon1715.java to BJ_1715_카드정렬하기.java

--- BJ_1715_카드정렬하기.java
+++ BJ_1715_카드정렬하기.java
@@ -9,7 +9,7 @@ import java.util.PriorityQueue;
 // 이 과정을 반복. 이를 위해 우선순위 큐를 사용하여
 // 값이 가장 작은 두 수를 더하고 다시 넣기
 // queue의 사이즈가 1이 될 때까지 반복
-public class Baekjoon1715 {
+public class BJ_1715_카드정렬하기 {
 	
 	static int N;
 	static long cnt;
