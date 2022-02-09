4739baa570e5c746da7c56dbf473393df4c9ccde
hyunmin2439 <72420501+hyunmin2439@users.noreply.github.com>
2022-02-09 23:46:16 +0900

    Add files via upload

---
+++ BJ_2587_대표값2.java
@@ -0,0 +1,29 @@
+package no_upload;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BJ_2587_대표값2 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int max = 0;
+		int[] arr = new int[5];
+		
+		for(int i = 0; i < 5; i++) {
+			arr[i] = Integer.parseInt(in.readLine());
+			max += arr[i];
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.println(max / 5);
+		System.out.println(arr[2]);
+		
+		in.close();
+		
+	}
+
+}
