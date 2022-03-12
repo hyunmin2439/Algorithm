696c57824bdef637a0305b294010a260bdeb6694
김현민 <72420501+hyunmin2439@users.noreply.github.com>
2022-03-12 21:18:42 +0900

    Update and rename BJ_1로만들기2.java to BJ_12852_1로만들기2.java

--- BJ_12852_1로만들기2.java
+++ BJ_12852_1로만들기2.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class BJ_1로만들기2 {
+public class BJ_12852_1로만들기2 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
