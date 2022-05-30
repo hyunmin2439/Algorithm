e7ab62c9f12d2bb918219bd8515df264d29b2770
김현민 <72420501+hyunmin2439@users.noreply.github.com>
2022-05-30 18:03:05 +0900

    [Gold IV] Title: 운동, Time: 792 ms, Memory: 62472 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	private static final int INF = 4_000_001;
+	private static int V;
+	
+	public static void main(String[] args) throws Exception {
+		int[][] dist = input();
+		floyd(dist);
+		int answer = getMinDistCycle(dist);
+		System.out.print(answer >= INF ? -1 : answer);
+	}
+	
+	private static int[][] input() throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+		
+		int[][] dist = new int[V + 1][V + 1];
+		for(int i = 1; i <= V; i++) {
+			for(int j = 1; j <= V; j++) {
+				dist[i][j] = INF;
+			}
+		}
+		
+		int a, b, c;
+		for(int i = 0; i < E; i++) {
+			st = new StringTokenizer(in.readLine());
+			
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			c = Integer.parseInt(st.nextToken());
+			
+			dist[a][b] = c;
+		}
+		
+		in.close();
+		
+		return dist;
+	}
+	
+	private static void floyd(int[][] dist) {
+		for(int k = 1; k <= V; k++) {
+			for(int i = 1; i <= V; i++) {
+				if(k == i) continue;
+				for(int j = 1; j <= V; j++) {
+					if(k == j || i == j) continue;
+					
+					dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+				}
+			}
+		}
+	}
+	
+	private static int getMinDistCycle(int[][] dist) {
+		int minDist = INF;
+		
+		for(int i = 1; i <= V; i++) {
+			for(int j = 1; j <= V; j++) {
+				minDist = Math.min(minDist, dist[i][j] + dist[j][i]);
+			}
+		}
+		
+		return minDist;
+	}
+}
