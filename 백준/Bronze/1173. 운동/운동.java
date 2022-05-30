04551e911415b1958994ce3222da141f14370b04
김현민 <72420501+hyunmin2439@users.noreply.github.com>
2022-05-30 18:38:40 +0900

    [Bronze II] Title: 운동, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/1173. 운동/운동.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	private static int N, m, M, T, R;
+	
+	public static void main(String[] args) throws Exception {
+		input();
+		
+		int time = m + T > M ? -1 : solve();
+		
+		System.out.print(time);
+	}
+	
+	private static int solve() {
+		int time = 0, timeExercised = 0, curr = m;
+		
+		while(timeExercised < N) {
+			if(curr + T <= M) {
+				curr += T;
+				timeExercised++;
+			}
+			else {
+				curr -= R;
+				curr = curr > m ? curr : m;
+			}
+			time++;
+		}
+		
+		return time;
+	}
+	
+	private static void input() throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		N = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		T = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		
+	}
+}
