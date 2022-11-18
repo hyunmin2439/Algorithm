ee41fcc8f4973cc06490e2fda0afae6b618e1534
김현민 <72420501+hyunmin2439@users.noreply.github.com>
2022-11-18 12:06:55 +0900

    [level 4] Title: 호텔 방 배정, Time: 315.80 ms, Memory: 151 MB -BaekjoonHub

--- 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.java
+++ 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.java
@@ -4,23 +4,20 @@ import java.util.HashMap;
 class Solution {
     public long[] solution(long k, long[] room_number) {
         int idx = -1, len = room_number.length;
-        long room = 0;
         Map<Long, Long> map = new HashMap<>();
         
         while(++idx < len) {
-            room = room_number[idx];
-            
-            room = find(map, room);
-            map.put(room, find(map, room + 1));
-            
-            room_number[idx] = room;
+            room_number[idx] = find(map, room_number[idx]);
         }
         
         return room_number;
     }
     
     public long find(Map<Long, Long> map, long find) {
-        if( !map.containsKey(find) ) return find;
+        if( !map.containsKey(find) ) {
+            map.put(find, find + 1);
+            return find;
+        }
         
         long parent = find(map, map.get(find));
         map.put(find, parent);
