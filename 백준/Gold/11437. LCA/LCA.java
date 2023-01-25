7f5b755cbd58ef92b1fe9f2f32e4a823cfbdf0b6
김현민 <72420501+hyunmin2439@users.noreply.github.com>
2023-01-25 15:28:47 +0900

    [Gold III] Title: LCA, Time: 504 ms, Memory: 48592 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.java
+++ 백준/Gold/11437. LCA/LCA.java
@@ -14,12 +14,14 @@ public class Main {
 		StringTokenizer st;
 		
 		List<List<Integer>> list = new ArrayList<>();
-		int N, M, a, b, depth[], parent[];
+		int N, M, a, b, maxE, depth[], parent[][];
 		
 		N = Integer.parseInt(in.readLine());
 		
+		maxE = (int)Math.ceil(log2(N));
 		depth = new int[N + 1];
-		parent = new int[N + 1];
+		parent = new int[N + 1][maxE];
+		depth[0] = -1; // dfs로 트리 만드고 depth 계산시 depth[1]을 0으로 만들기 위함
 
 		for(int i = 0; i <= N; i++)
 			list.add(new ArrayList<>());
@@ -32,7 +34,14 @@ public class Main {
 			list.get(b).add(a);
 		}
 		
-		dfs(list, new boolean[N + 1], parent, depth, 1, 0);
+		dfs(list, new boolean[N + 1], parent, depth, 1);
+		
+		// 2^k 부모들 세팅
+		for(int k = 1; k < maxE; k++) {	
+			for(int i = 1; i <= N; i++) {
+				parent[i][k] = parent[parent[i][k - 1]][k - 1];
+			}
+		}
 		
 		M = Integer.parseInt(in.readLine());
 		
@@ -41,46 +50,59 @@ public class Main {
 			a = Integer.parseInt(st.nextToken());
 			b = Integer.parseInt(st.nextToken());
 			
-			out.write(lca(parent, depth, a, b) + "\n");
+			out.write(lca(parent, depth, a, b, maxE) + "\n");
 		}
 		
 		out.flush();
 	}
 	
-	private static void dfs(List<List<Integer>> list, boolean[] visited, int[] parent, int[] depth, int curr, int cnt) {
-		List<Integer> children = list.get(curr);
+	private static double log2(double x) {
+		return Math.log(x) / Math.log(2);
+	}
+	
+	private static void dfs(List<List<Integer>> list, boolean[] visited, int[][] parent, int[] depth, int curr) {
 		int child;
 		
-		depth[curr] = cnt;
+		depth[curr] = depth[parent[curr][0]] + 1;
 		visited[curr] = true;
 		
+		List<Integer> children = list.get(curr);
 		for(int i = 0; i < children.size(); i++) {
 			child = children.get(i);
 			
 			if(visited[child]) continue;
 			
-			parent[child] = curr;
-			dfs(list, visited, parent, depth, child, cnt + 1);
+			parent[child][0] = curr;
+			dfs(list, visited, parent, depth, child);
 		}
 	}
 	
-	private static int lca(int[] parent, int[] depth, int a, int b) {
-		int tmp = a;
-		
-		// depth가 큰쪽이 항상 a가 되도록 변경
-		if(depth[a] < depth[b]) {
+	private static int lca(int[][] parent, int[] depth, int a, int b, int maxE) {
+		// depth가 큰쪽이 항상 b가 되도록 변경
+		if(depth[a] > depth[b]) {
+			int tmp = a;
 			a = b;
 			b = tmp;
 		}
 		
-		while(depth[a] > depth[b]) a = parent[a];
+		for(int e = maxE - 1; e >= 0; e--) {
+			if(depth[b] - depth[a] >= (1 << e)) {
+				b = parent[b][e];
+			}
+		}
+
 		
-		while(a != b) {
-			a = parent[a];
-			b = parent[b];
+		if(a == b)
+			return a; // 같으면 a 리턴
+			
+		for(int e = maxE - 1; e >= 0; e--) {
+			if(parent[a][e] != parent[b][e]) {
+				a = parent[a][e];
+				b = parent[b][e];
+			}
 		}
 		
-		return a;
+		return parent[a][0];
 	}
 
 }
