c74de7d579cf43be9744939b2668830ddb4f529c
김현민 <72420501+hyunmin2439@users.noreply.github.com>
2023-02-02 21:54:12 +0900

    [D5] Title: 촛불 이벤트, Time: 264 ms, Memory: 50,388 KB -BaekjoonHub

--- SWEA/D5/9843. 촛불 이벤트/촛불 이벤트.java
+++ SWEA/D5/9843. 촛불 이벤트/촛불 이벤트.java
@@ -11,32 +11,35 @@ public class Solution {
 		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		
-		long N, mid, low, high;
+		long N, ans, tmp;
 		int t = 0, T;
 		
 		T = Integer.parseInt(in.readLine());
 		
 		while(t++ < T) {
 			N = Long.parseLong(in.readLine());
-			
-			low = 1;
-			high = Math.min(N, 1_414_213_562); // 10^18을 넘는 최소값
-			
-			while(low < high) {
-				// 오버플로우의 위험 방지 및 low + high 음수 시 나누기 2 다르게 동작 방지
-				mid = low + ((high - low) >> 1);
-				
-				if((mid * (mid + 1)) >> 1 < N) // 조건
-					low = mid + 1;
-				else
-					high = mid;
-			}
-			
-			mid = (low * (low + 1)) >> 1;
-			sb.append("#").append(t).append(" ").append(mid == N ? low : -1).append("\n");
+			ans = binarySearch(N);
+			tmp = (ans * (ans + 1)) >> 1;
+			sb.append("#").append(t).append(" ").append(tmp == N ? ans : -1).append("\n");
 		}
 		
 		out.write(sb.toString());
 		out.flush();		
+	}
+	
+	private static long binarySearch(long N) {
+		long mid, low = 1, high = Math.min(N, 1_414_213_562); // 10^18을 넘는 최소값
+		
+		while(low < high) {
+			// 오버플로우의 위험 방지 및 low + high 음수 시 나누기 2 다르게 동작 방지
+			mid = low + ((high - low) >> 1);
+			
+			if((mid * (mid + 1)) >> 1 < N) // 조건
+				low = mid + 1;
+			else
+				high = mid;
+		}
+		
+		return low;
 	}
 }
