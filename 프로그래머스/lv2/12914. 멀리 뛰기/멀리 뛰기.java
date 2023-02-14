eb7d04eb8d011ea8e4cf217790fc4a11ef013757
김현민 <72420501+hyunmin2439@users.noreply.github.com>
2023-02-14 23:38:47 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.16 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(int n) {
+        long MOD = 1_234_567L;
+        long[] dp = new long[n + 1];
+        dp[0] = dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+        
+        return dp[n];
+    }
+}
